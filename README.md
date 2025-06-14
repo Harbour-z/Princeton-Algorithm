8308d557058f5c571cba8e18206f86019365558b
Zhidong Zhang <814287468@qq.com>
2025-06-14 13:25:41 +0800

    change readme and change structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Princeton-Algorithm
+
+This repository contains my solutions and learning records for the course Algorithm I & II on coursera.
---
+++ projects/helloworld_0/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * @author Zhidong Zhang
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
